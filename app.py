c0c70bae53f115505792266c96e58da3bd96eb18
scubastevew77 <147108604+scubastevew77@users.noreply.github.com>
2023-12-15 09:31:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard - updated by Steve", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 @st.cache_data
@@ -32,7 +32,7 @@ df = get_data_from_excel()
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
 city = st.sidebar.multiselect(
-    "Select the City:",
+    "Select the City from here:",
     options=df["City"].unique(),
     default=df["City"].unique()
 )
