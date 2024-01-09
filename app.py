0a29385e54106f0c4e28f3d86ea8d98c8c85c14e
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-01-09 13:57:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard from not main", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 @st.cache_data
@@ -59,7 +59,7 @@ if df_selection.empty:
     st.stop() # This will halt the app from further execution.
 
 # ---- MAINPAGE ----
-st.title(":bar_chart: Sales Dashboard")
+st.title(":bar_chart: Sales Dashboard from not main")
 st.markdown("##")
 
 # TOP KPI's
